d73e9f16d1798e73842cf4efce37ac2b68cda9a0
HeatherDa <crystal_tree@hotmail.com>
2017-02-18 15:13:33 -0600

    First Commit, basically done

--- Simple Calculator/Form1.Designer.cs
+++ Simple Calculator/Form1.Designer.cs
@@ -28,12 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtOp1 = new System.Windows.Forms.TextBox();
+            this.txtOper = new System.Windows.Forms.TextBox();
+            this.txtOp2 = new System.Windows.Forms.TextBox();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Operand 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Operator:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Operand 2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Result:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(33, 187);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(129, 187);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtOp1
+            // 
+            this.txtOp1.Location = new System.Drawing.Point(104, 33);
+            this.txtOp1.Name = "txtOp1";
+            this.txtOp1.Size = new System.Drawing.Size(100, 20);
+            this.txtOp1.TabIndex = 1;
+            this.txtOp1.TextChanged += new System.EventHandler(this.textChange);
+            // 
+            // txtOper
+            // 
+            this.txtOper.Location = new System.Drawing.Point(104, 67);
+            this.txtOper.Name = "txtOper";
+            this.txtOper.Size = new System.Drawing.Size(30, 20);
+            this.txtOper.TabIndex = 3;
+            this.txtOper.TextChanged += new System.EventHandler(this.textChange);
+            // 
+            // txtOp2
+            // 
+            this.txtOp2.Location = new System.Drawing.Point(104, 103);
+            this.txtOp2.Name = "txtOp2";
+            this.txtOp2.Size = new System.Drawing.Size(100, 20);
+            this.txtOp2.TabIndex = 5;
+            this.txtOp2.TextChanged += new System.EventHandler(this.textChange);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(104, 142);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.ReadOnly = true;
+            this.txtAnswer.Size = new System.Drawing.Size(100, 20);
+            this.txtAnswer.TabIndex = 7;
+            this.txtAnswer.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(239, 235);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.txtOp2);
+            this.Controls.Add(this.txtOper);
+            this.Controls.Add(this.txtOp1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Simple Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtOp1;
+        private System.Windows.Forms.TextBox txtOper;
+        private System.Windows.Forms.TextBox txtOp2;
+        private System.Windows.Forms.TextBox txtAnswer;
     }
 }
 
--- Simple Calculator/Form1.cs
+++ Simple Calculator/Form1.cs
@@ -16,5 +16,150 @@ namespace Simple_Calculator
         {
             InitializeComponent();
         }
+
+
+        private void btnCalculate_Click(object sender, EventArgs e)//calculate answer
+        {
+            try
+            {
+                TextBox[] operands = new TextBox[2] { txtOp1, txtOp2 };
+                if (dataValidation(operands))//check for data validity
+                {
+                    decimal op1 = Convert.ToDecimal(txtOp1.Text);
+                    decimal op2 = Convert.ToDecimal(txtOp2.Text);
+                    string oper = txtOper.Text;
+
+                    decimal answer=calculate(op1, oper, op2);
+                    txtAnswer.Text = Convert.ToString(answer); //another way to do this would be an if statement where txtAnswer is set depending on whether /by0 bool was set to true.
+                    divideByZero(); //handle divide by zero if it happened.
+                }
+            }
+            catch(Exception ex)//not really sure whay kind of exception I'm expecting.
+            {
+                MessageBox.Show(ex.Message + "\n\n" + ex.GetType().ToString());
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)//exit button
+        {
+            this.Close();
+        }
+        public bool IsPresent (TextBox textBox, string name) //is there data in the text box?
+        {
+            if (textBox.Text == "")
+            {
+                MessageBox.Show(name + " is a required field.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+        public bool IsNumber(TextBox textBox, string name) //is the data numeric?
+        {
+            decimal numdec = 0m;
+            if (Decimal.TryParse(textBox.Text, out numdec))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(name + " must be a number.", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+        }
+        public bool IsInRange(TextBox textBox, string name, decimal min, decimal max) //is the number in the right range?
+        {
+            decimal number = Convert.ToDecimal(textBox.Text);
+            if (number < min || number > max)
+            {
+                MessageBox.Show(name + " must be between " + min.ToString() + " and "+ max.ToString() + ".", "Entry Error");
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+        private bool IsOperator()//check if the operator is valid
+        {
+            string oper = txtOper.Text.Trim();
+            if (!IsPresent(txtOper, "Operator")) return false;//check that the text box contains something
+            else if (oper == "*" || oper == "/" || oper == "-" || oper == "+") return true; //check that it contains the right thing
+            else //what to do if the textbox contains the wrong thing
+            {
+                MessageBox.Show("Operator must be *, /, -, or +.", "Entry Error");
+                txtOper.Focus();
+                return false;
+            }
+        }
+        private void divideByZero()//test for divide by zero and return error message
+        {
+            if (txtOper.Text == "/" && (txtOp2.Text == "0"))
+            {
+                txtAnswer.Text="";
+                MessageBox.Show("Cannot divide by zero.", "Entry Error");
+                txtOp2.Focus();
+            }
+        }
+        private bool dataValidation(TextBox[] textBoxes)//can only return one error message at a time. (what if both textboxes wrong?)
+        {
+            
+            foreach(TextBox t in textBoxes)
+            {
+                string name = "";
+                if (t.Name.Contains("1")) name = "Operand 1";
+                else if (t.Name.Contains("2")) name = "Operand 2";
+                if (!(IsPresent(t, name) && IsNumber(t, name) && IsInRange(t, name, 0, 1000000) && IsOperator()))
+                {
+                    return false; //if any txtbox evaluates to false, stop checking.
+                }
+            }
+
+            return true; //should only reach this if they are all true
+
+        }
+
+        private void textChange(object sender, EventArgs e)
+        {
+            //if text is changed in any textbox, clear answer textbox
+            txtAnswer.Text = "";
+        }
+
+        private decimal calculate(decimal operand1, string operator1, decimal operand2)
+        {
+            decimal answer = 0;
+            try
+            {
+                
+                switch (operator1)
+                {
+                    case "/":
+                        if (operand2 == 0) break; //exit loop without calculating or assigning a value if it would divide by zero
+                        else answer = operand1 / operand2;
+                        return answer;
+                    case "*":
+                        answer = operand1 * operand2;
+                        return answer;
+                    case "-":
+                        answer = operand1 - operand2;
+                        return answer;
+                    case "+":
+                        answer = operand1 + operand2;
+                        return answer;
+                }
+                answer = Math.Round(answer, 4);//round to 4 decimal places
+            }
+            catch(OverflowException)//this should never happen, because of data validation.
+            {
+                MessageBox.Show("An overflow exception has occured.  Please enter smaller values.", "Entry Error");
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+            }
+            return answer; //returns decimal.  
+            //I originally wrote it to set txtAnswer.Text directly, but changed it because of the instructions.
+            //Is there a reason why it's better to return the decimal and then assign it?
+        }
     }
 }
